1a95be8a83f3b54e1cae04c823dab3f32e345ad5
h3nnn4n <uber.renan@gmail.com>
2022-01-31 12:12:16 -0300

    Update metadata + set version to 1.1.0

--- django_cprofile_middleware/metadata.py
+++ django_cprofile_middleware/metadata.py
@@ -1,4 +1,4 @@
 # All values must be strings and quoted with singlequote (').
-__version__ = '1.0.6'
+__version__ = '1.1.0'
 __author__ = 'Omar Bohsali, Renan S Silva'
-__authoremail__ = 'me@omarish.com'
+__authoremail__ = 'me@omarish.com, me@h3nnn4n.me'
